313c49085e3f13cb8d24aaf2b85d67b014c7988a
Jakob K <dev@jakobk.de>
2022-04-29 20:57:13 +0200

    Handle IndirectEntityDamageSource in protection

--- src/main/kotlin/net/axay/kotlinmchub/features/Protection.kt
+++ src/main/kotlin/net/axay/kotlinmchub/features/Protection.kt
@@ -3,6 +3,7 @@ package net.axay.kotlinmchub.features
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.damagesource.EntityDamageSource
+import net.minecraft.world.damagesource.IndirectEntityDamageSource
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.decoration.ArmorStand
 import net.minecraft.world.entity.decoration.ItemFrame
@@ -11,14 +12,10 @@ import net.minecraft.world.entity.projectile.Projectile
 
 fun Entity.isProtected(damageSource: DamageSource): Boolean {
     if (this is Player) {
-        if (damageSource is EntityDamageSource) {
-            val attacker = damageSource.entity
-            if (attacker is ServerPlayer && attacker.gameMode.isSurvival) {
-                return true
-            }
-            return false
+        return if (damageSource is EntityDamageSource) {
+            damageSource.entity.isSurvivalPlayer()
         } else {
-            return when (damageSource) {
+            when (damageSource) {
                 DamageSource.CACTUS -> true
                 else -> false
             }
@@ -26,12 +23,17 @@ fun Entity.isProtected(damageSource: DamageSource): Boolean {
     }
 
     if (damageSource is EntityDamageSource) {
-        val attacker = damageSource.entity
-        when {
-            attacker is ServerPlayer && attacker.gameMode.isSurvival -> return true
-            attacker is Projectile && (this is ItemFrame || this is ArmorStand) -> return true
+        if (damageSource.entity?.isSurvivalPlayer() == true)
+            return true
+
+        if (damageSource is IndirectEntityDamageSource) {
+            val attacker = damageSource.directEntity
+            if (attacker is Projectile && (this is ItemFrame || this is ArmorStand))
+                return true
         }
     }
 
     return false
 }
+
+private fun Entity?.isSurvivalPlayer() = this is ServerPlayer && gameMode.isSurvival
