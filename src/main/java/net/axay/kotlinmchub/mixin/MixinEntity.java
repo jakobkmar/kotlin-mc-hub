1b9746903c70de9ffe6186ca8a7be5f28b68467e
mooziii <63669478+mooziii@users.noreply.github.com>
2022-04-30 12:11:20 +0200

    Rename MixinEntityHurt and make every player glow
    ist es nicht eig. `EntityMixin`

--- src/main/java/net/axay/kotlinmchub/mixin/MixinEntity.java
+++ src/main/java/net/axay/kotlinmchub/mixin/MixinEntity.java
@@ -3,13 +3,14 @@ package net.axay.kotlinmchub.mixin;
 import net.axay.kotlinmchub.features.ProtectionKt;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
-public class MixinEntityHurt {
+public class MixinEntity {
 
     @Inject(
         method = "isInvulnerableTo",
@@ -22,4 +23,18 @@ public class MixinEntityHurt {
             cir.setReturnValue(true);
         }
     }
+
+    @Inject(
+            method = "isCurrentlyGlowing",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void makeEverythingGlow(CallbackInfoReturnable<Boolean> cir) {
+        if((Object) this instanceof Player) { //yes it says it is impossible but it actually is possible.
+            cir.setReturnValue(true);
+        }
+    }
+
+
+
 }
