a3be3dfdf8fdecc68cdca25d2d15cc4e70e5574d
Amin Haddou <amin.haddou@frg.wwschool.de>
2022-04-30 13:10:18 +0200

    remove unnecessary data for motion packet

--- src/main/kotlin/net/axay/kotlinmchub/features/Trident.kt
+++ src/main/kotlin/net/axay/kotlinmchub/features/Trident.kt
@@ -4,11 +4,10 @@ import net.axay.fabrik.core.entity.modifyVelocity
 import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.phys.Vec3
 
 fun onTridentRelease(player: Player, x: Double, y: Double, z: Double) {
     if(player is ServerPlayer) {
         player.modifyVelocity(x, y, z, false)
-        player.connection.send(ClientboundSetEntityMotionPacket(player.id, Vec3(x, y, z)))
+        player.connection.send(ClientboundSetEntityMotionPacket(player))
     }
 }
